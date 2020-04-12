8496b7f40473db1b29d81fda96f9fb1c6934d840
Danilo Acceto Kasparian <dacceto@gmail.com>
2020-04-12 20:32:51 -0300

    Add exception

--- OneExpense.API/Startup.cs
+++ OneExpense.API/Startup.cs
@@ -37,10 +37,10 @@ namespace OneExpense.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
-            }
+            //}
 
             //app.UseHttpsRedirection();
 
