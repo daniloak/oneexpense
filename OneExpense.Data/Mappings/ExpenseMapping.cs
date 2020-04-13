30abf5de75f25e0205bad1810efe6c5d112065c6
Danilo Acceto Kasparian <dacceto@gmail.com>
2020-04-12 22:43:08 -0300

    Change mapping

--- OneExpense.Data/Mappings/ExpenseMapping.cs
+++ OneExpense.Data/Mappings/ExpenseMapping.cs
@@ -9,12 +9,15 @@ namespace OneExpense.Data.Mappings
         public void Configure(EntityTypeBuilder<Expense> builder)
         {
             builder.HasKey(p => p.Id);
-            builder.Property(p => p.Id)
-                .HasDefaultValueSql("uuid_generate_v4()");
+            builder.Property(p => p.Id).HasDefaultValueSql("uuid_generate_v4()");
 
             builder.Property(p => p.Description)
                 .IsRequired()
-                .HasColumnType("varchar(250)");
+                .HasColumnType("varchar(200)");
+
+            builder.HasMany(f => f.Details)
+            .WithOne(p => p.Expense)
+            .HasForeignKey(p => p.ExpenseId);
 
             builder.Property(p => p.Amount)
                 .IsRequired();
