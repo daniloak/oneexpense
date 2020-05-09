464245d743b05cf444a93bc828f3871b7e750514
Danilo Kasparian <danilo.kasparian@duvaltex.com>
2020-05-09 13:01:42 -0400

    Add identity

---
+++ OneExpense.Identity/Controllers/MainController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace OneExpense.Identity.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        protected ICollection<string> Errors = new List<string>();
+
+        protected ActionResult ApiResponse(object result = null)
+        {
+            if (IsValid())
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(new ValidationProblemDetails(new Dictionary<string, string[]>
+            {
+                { "Details", Errors.ToArray() }
+            }));
+        }
+
+        protected ActionResult ApiResponse(ModelStateDictionary modelState)
+        {
+            var errors = modelState.Values.SelectMany(e => e.Errors);
+            foreach (var error in errors)
+            {
+                AddError(error.ErrorMessage);
+            }
+
+            return ApiResponse();
+        }
+
+        protected bool IsValid() => !Errors.Any();
+        protected void AddError(string error) => Errors.Add(error);
+        protected void ClearErrors() => Errors.Clear();
+    }
+}
